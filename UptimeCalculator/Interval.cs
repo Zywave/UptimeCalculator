ec7637218c7c546e43505418c2767c7df89a8455
John Cruikshank <cruikshj@live.com>
2017-02-09 16:56:56 -0600

    feat: interval parsing and type converter

--- UptimeCalculator/Interval.cs
+++ UptimeCalculator/Interval.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
 
 namespace UptimeCalculator
 {
+    [TypeConverter(typeof(IntervalConverter))]
     public struct Interval : IEquatable<Interval>, IFormattable
     {
         public Interval(DateTimeOffset start, DateTimeOffset end)
@@ -34,17 +37,66 @@ namespace UptimeCalculator
 
         public override string ToString()
         {
-            return $"{Start} -> {End}";
+            return $"{Start},{End}";
         }
 
         public string ToString(string format, IFormatProvider formatProvider)
         {
-            return $"{Start.ToString(format, formatProvider)} -> {End.ToString(format, formatProvider)}";
+            return $"{Start.ToString(format, formatProvider)},{End.ToString(format, formatProvider)}";
         }
 
         public override int GetHashCode()
         {
             return Start.GetHashCode() ^ End.GetHashCode();
+        }
+
+        public static Interval Parse(string input)
+        {
+            if (input == null) throw new ArgumentNullException(nameof(input));
+
+            Interval interval;
+            if (TryParse(input, out interval))
+            {
+                return interval;
+            }
+
+            throw new FormatException("The input does not represent a valid interval.");
+        }
+
+        public static bool TryParse(string input, out Interval interval)
+        {
+            interval = default(Interval);
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return false;
+            }
+
+            var parts = input.Split(',');
+            if (parts.Length != 2)
+            {
+                return false;
+            }
+
+            DateTimeOffset start, end;
+
+            if (!DateTimeOffset.TryParse(parts[0], out start))
+            {
+                return false;
+            }
+
+            if (!DateTimeOffset.TryParse(parts[1], out end))
+            {
+                return false;
+            }
+
+            if (end < start)
+            {
+                return false;
+            }
+
+            interval = new Interval(start, end);
+            return true;
         }
     }
 }
---
+++ UptimeCalculator/IntervalConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.ComponentModel;
+
+namespace UptimeCalculator
+{
+    public class IntervalConverter : TypeConverter
+    {
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            if (sourceType == typeof(string))
+            {
+                return true;
+            }
+            return base.CanConvertFrom(context, sourceType);
+        }
+
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            if (value is string)
+            {
+                Interval interval;
+                if (Interval.TryParse((string)value, out interval))
+                {
+                    return interval;
+                }
+            }
+            return base.ConvertFrom(context, culture, value);
+        }
+    }
+}
